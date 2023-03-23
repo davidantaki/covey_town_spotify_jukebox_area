9f0b8acbc361ff9d04213d90387b9075e6a2a79f
John Nahill <johnnahill@Johns-MacBook-Pro.local>
2023-03-23 12:36:09 -0400

    Modified constructor and made song fields private

--- covey.town/townService/src/lib/Song.ts
+++ covey.town/townService/src/lib/Song.ts
@@ -1,41 +1,89 @@
 class Song {
   // The title of the song
-  title: string;
+  private _title: string;
 
-  // The artist of the song
-  artist: string;
+  // The artists of the song
+  private _artists: [];
 
   // The spotifyID of the song
-  spotifyId: string;
+  private _spotifyId: string;
 
   // The id of the player that added the song
-  addedBy: string;
+  private _addedBy: string;
 
   // The number of upvotes the song has received
-  upvotes: number;
+  private _upvotes: number;
 
   // The number of downvotes the song has received
-  downvotes: number;
+  private _downvotes: number;
 
   // The JSON for the song that is received from getSong
-  songJson: string;
+  private _songJson: any;
 
-  constructor(
-    title: string,
-    artist: string,
-    spotifyId: string,
-    addedBy: string,
-    upvotes: number,
-    downvotes: number,
-    songJson: string,
-  ) {
-    this.title = title;
-    this.artist = artist;
-    this.spotifyId = spotifyId;
-    this.addedBy = addedBy;
-    this.upvotes = upvotes;
-    this.downvotes = downvotes;
-    this.songJson = songJson;
+  constructor(addedBy: string, songJson: any) {
+    this._title = songJson.name;
+    this._artists = songJson.artists.map((artist: { name: string }) => artist.name);
+    this._spotifyId = songJson.id;
+    this._addedBy = addedBy;
+    this._upvotes = 0;
+    this._downvotes = 0;
+    this._songJson = songJson;
+  }
+
+  get title(): string {
+    return this._title;
+  }
+
+  set title(title: string) {
+    this._title = title;
+  }
+
+  get artists(): [] {
+    return this._artists;
+  }
+
+  set artists(artists: []) {
+    this._artists = artists;
+  }
+
+  get spotifyId(): string {
+    return this._spotifyId;
+  }
+
+  set spotifyId(spotifyId: string) {
+    this._spotifyId = spotifyId;
+  }
+
+  get addedBy(): string {
+    return this._addedBy;
+  }
+
+  set addedBy(addedBy: string) {
+    this._addedBy = addedBy;
+  }
+
+  get upvotes(): number {
+    return this._upvotes;
+  }
+
+  set upvotes(upvotes: number) {
+    this._upvotes = upvotes;
+  }
+
+  get downvotes(): number {
+    return this._downvotes;
+  }
+
+  set downvotes(downvotes: number) {
+    this._downvotes = downvotes;
+  }
+
+  get songJson(): string {
+    return this._songJson;
+  }
+
+  set songJson(songJson: string) {
+    this._songJson = songJson;
   }
 
   getNetVotes(): number {

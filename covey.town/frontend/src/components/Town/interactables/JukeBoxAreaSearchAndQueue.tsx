cc6c7d5515161e95046d501394472c0dd2049a89
nezamj03 <jazayeri.ne@northeastern.edu>
2023-03-29 17:15:25 -0400

    search endpoint queries everything correctly

--- covey.town/frontend/src/components/Town/interactables/JukeBoxAreaSearchAndQueue.tsx
+++ covey.town/frontend/src/components/Town/interactables/JukeBoxAreaSearchAndQueue.tsx
@@ -13,14 +13,13 @@ import {
   useToast,
   VStack,
 } from '@chakra-ui/react';
-import { encode as base64_encode } from 'base-64';
 import React, { useCallback, useEffect, useState } from 'react';
 import ReactPlayer from 'react-player';
 import { useInteractable, useJukeBoxAreaController } from '../../../classes/TownController';
 import useTownController from '../../../hooks/useTownController';
+import SpotifyController from '../../../spotify/SpotifyController';
 import JukeBoxAreaInteractable from './JukeBoxArea';
 
-const ALLOWED_DRIFT = 3;
 export class MockReactPlayer extends ReactPlayer {
   render(): React.ReactNode {
     return <></>;
@@ -36,17 +35,6 @@ export function SearchResult({
   songArtist: string;
   songDuration: string;
 }): JSX.Element {
-  const property = {
-    imageUrl: 'https://bit.ly/2Z4KKcF',
-    imageAlt: 'Rear view of modern home with pool',
-    beds: 3,
-    baths: 2,
-    title: 'Modern home in city center in the heart of historic Los Angeles',
-    formattedPrice: '$1,900.00',
-    reviewCount: 34,
-    rating: 4,
-  };
-
   return (
     <Grid templateRows='repeat(1, 1fr)' templateColumns='repeat(3, 1fr)' gap={50} p='0'>
       <GridItem colSpan={1} h='10' bg='transparent'>
@@ -71,8 +59,6 @@ export function SearchResult({
 export function JukeBoxArea({
   jukeBoxArea,
 }: {
-  // isOpenInit: boolean;
-  // close: () => void;
   jukeBoxArea: JukeBoxAreaInteractable;
 }): JSX.Element {
   const townController = useTownController();
@@ -82,11 +68,6 @@ export function JukeBoxArea({
   const [searchResults, setSearchResults] = useState<any>();
   const handleSearchChange = (event: { target: { value: React.SetStateAction<string> } }) => {
     setSearchValue(event.target.value);
-    // SpotifyController.token('null');
-    // SpotifyController.search(searchValue, SpotifyController.token()).then((res) => {
-    setSearchResults(res);
-    //   console.log(res);
-    // }
   };
 
   const closeModal = useCallback(() => {
@@ -122,87 +103,23 @@ export function JukeBoxArea({
     onOpen();
   }
 
-  const [token, setToken] = useState<undefined | string>(undefined);
-
-  // useEffect(() => {
-  //   const hash = window.location.hash;
-  //   let localToken = window.localStorage.getItem('token');
-
-  //   if (!localToken && hash) {
-  //     localToken = hash
-  //       .substring(1)
-  //       .split('&')
-  //       .find(elem => elem.startsWith('access_token'))
-  //       .split('=')[1];
-
-  //     window.location.hash = '';
-  //     window.localStorage.setItem('token', localToken);
-  //   }
-
-  //   setToken(localToken);
-  // }, []);
-
-  // const logout = () => {
-  //   setToken('');
-  //   window.localStorage.removeItem('token');
-  // };
-
-  // const spotifyLogin = useCallback(async () => {
-  //   try {
-  //     await SpotifyController.login();
-  //     townController.unPause();
-  //     closeModal();
-  //   } catch (err) {
-  //     if (err instanceof Error) {
-  //       toast({
-  //         title: 'Unable to create conversation',
-  //         description: err.toString(),
-  //         status: 'error',
-  //       });
-  //     } else {
-  //       console.trace(err);
-  //       toast({
-  //         title: 'Unexpected Error',
-  //         status: 'error',
-  //       });
-  //     }
-  //   }
-  // }, [closeModal, townController, toast]);
-
-  // spotifyLogin();
-
-  // const REACT_APP_TOWNS_SERVICE_URL = 'http://localhost:8081';
-  const id = '1d5bdd45d42c4c92a2a935346a2fc3e2';
-  const secret = '5c47a4ccaa1047ad8ca79e76a21d03f5';
-  // const SPOTIFY_REDIRECT_URI = 'http://localhost:8888/callback';
-  // const SPOTIFY_AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
-  // const SPOTIFY_RESPONSE_TYPE = 'token';
+  const [token, setToken] = useState<string>('');
 
   useEffect(() => {
-    async function fetchToken() {
-      const getToken = async () => {
-        try {
-          const response = await fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            headers: {
-              'Authorization': 'Basic ' + base64_encode(id + ':' + secret),
-              'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: 'grant_type=client_credentials',
-          });
-          const data = await response.json();
-          const auth = data.access_token;
-          console.log(auth);
-          return auth;
-        } catch (error) {
-          console.log(error);
-        }
-      };
-      const auth = await getToken();
+    async function getToken() {
+      const auth = await SpotifyController.fetchToken();
       setToken(() => auth);
     }
-    fetchToken();
+    getToken();
   }, []);
+
+  useEffect(() => {
+    async function findSongs() {
+      const songs = await SpotifyController.search(token, searchValue, 'track');
+      console.log(songs);
+    }
+    findSongs();
+  }, [searchValue, token]);
 
   return (
     <>

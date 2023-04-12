7bf943841a3106e9493262f86f153238806dbbcf
Iconoclastic0428 <li.shngq@gmail.com>
2023-04-12 01:15:11 -0400

    frontend testing

---
+++ frontend/src/classes/JukeBoxAreaController.test.ts
@@ -0,0 +1,55 @@
+import TownController from './TownController';
+import JukeBoxAreaController, { JukeBoxAreaEvents, Song } from './JukeBoxAreaController';
+import { MockProxy, mock, mockClear } from 'jest-mock-extended';
+import { JukeBoxArea } from '../types/CoveyTownSocket';
+
+describe('Jukebox Song Queue', () => {
+  let testJukeBoxArea: JukeBoxArea;
+  let testJukeBoxController: JukeBoxAreaController;
+  const townController: MockProxy<TownController> = mock<TownController>();
+  const mockListeners = mock<JukeBoxAreaEvents>();
+
+  beforeEach(() => {
+    testJukeBoxArea = {
+      id: 'testArea',
+      songQueue: [
+        {
+          title: 'test',
+          artists: ['test'],
+          spotifyId: 'spotify:id',
+          addedBy: 'test',
+          upvotes: 0,
+          songJson: '{someJson: 0}',
+        },
+      ],
+    };
+    testJukeBoxController = new JukeBoxAreaController(testJukeBoxArea);
+    mockClear(townController);
+    mockClear(mockListeners.jukeBoxQueueChange);
+
+    testJukeBoxController.addListener('jukeBoxQueueChange', mockListeners.jukeBoxQueueChange);
+  });
+
+  describe('Song queue change', () => {
+    it('song queue change', () => {
+      const song: Song = {
+        title: 'another song',
+        artists: ['artist'],
+        spotifyId: 'some:spotify:id',
+        addedBy: 'tester',
+        upvotes: 1,
+        songJson: '{anotherJson: 0}',
+      };
+      const newQueue = [...testJukeBoxController.queue];
+      newQueue.push(song);
+      testJukeBoxController.queue = newQueue;
+      expect(mockListeners.jukeBoxQueueChange).toBeCalledWith(newQueue);
+      expect(testJukeBoxController.queue).toBe(newQueue);
+    });
+
+    it('should not emit a song queue change if nothing changed', () => {
+      testJukeBoxController.queue = testJukeBoxArea.songQueue;
+      expect(mockListeners.jukeBoxQueueChange).not.toBeCalled();
+    });
+  });
+});
